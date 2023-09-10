fa28c03b9e7faeb9251001d3d6d950da3e066d8c
Hoziefa <hoziefaalhasan@gmail.com>
2023-09-11 01:24:21 +0300

    Fix the issue of auto deleting the query.

--- src/lib/hooks/useQueryParams.ts
+++ src/lib/hooks/useQueryParams.ts
@@ -28,7 +28,7 @@ export const useQueryParams = (
   useEffect(() => {
     const url = new URLSearchParams(searchParams);
 
-    if (url.get(queryKey) === queryValue) return;
+    if (queryValue === '' || url.get(queryKey) === queryValue) return;
 
     if (queryValue) url.set(queryKey, queryValue);
     else url.delete(queryKey);
